9d8745f9db0b8994c4abcaf2ff9c33e593c33343
Dylan Jhaveri <djhaveri@mux.com>
2025-12-10 14:49:54 -0800

    remove nitro configuration file

--- nitro.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineNitroConfig } from "nitro/config";
-
-export default defineNitroConfig({
-  modules: ["workflow/nitro"],
-  compatibilityDate: "2024-01-01",
-  serverDir: "./test_server"
-});
