3fd9dd963feffd432ef696a5ef76a55cce9988a1
Phil <578330+philcluff@users.noreply.github.com>
2026-03-06 19:05:38 +0000

    Try 4 on evals

--- evalite.config.ts
+++ evalite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from "evalite/config";
 // See: https://v1.evalite.dev/guides/configuration
 export default defineConfig({
   testTimeout: 600000, // 600 seconds
-  maxConcurrency: 3, // Run up to 3 tests in parallel
+  maxConcurrency: 4, // Run up to 4 tests in parallel
   hideTable: false, // Show the table of results
   viteConfig: {
     resolve: {
